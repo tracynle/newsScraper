7bb39396cc7e75ebe3a729e0a92307327fc4f1c2
Tracy Le <tnle9@asu.edu>
2019-04-13 22:49:46 -0700

    Changed server.js file and edited process.en port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var cheerio = require("cheerio");
 var db = require("./models");
 
 // Assign your port
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
